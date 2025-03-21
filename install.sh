89c053ad17700ccc1ef591978f3b99b147d61705
connor <hq2831@gmail.com>
2025-03-21 18:23:31 +1000

    Setup testing

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-DOTFILES="$HOME/dotfiles"
-cwd=$PWD
-echo $cwd
+DOTFILES=$PWD
 # # Create symbolic links
-# ln -sf "$DOTFILES/bash/.bashrc" "$HOME/.bashrc"
-# ln -sf "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
-# ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
-# ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES/bash/.bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES/vim/.vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "Dotfiles installation complete!"
